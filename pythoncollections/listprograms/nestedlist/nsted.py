6a4f39d8e20b088ca6c9b113e4ee50b153a4e573
Akhil Francis <akhilfranci93@gmail.com>
2021-02-05 12:37:29 +0530

    pairs

---
+++ pythoncollections/listprograms/nestedlist/nsted.py
@@ -0,0 +1,24 @@
+employees=[
+    [100,"Akhil","developer","50000",1989,1995],
+    [101,"Arun","developer","45000",1970,1990],
+    [102,"pratheesh","qc","30000",1989,1991],
+    [103,"abi","ba","20000",1990,1999],
+]
+total=0
+sallist=[]
+for emp in employees:
+    #print(emp[1])
+
+    #print(emp[3])
+
+    #if emp[2]=="developer":
+        #print(emp)
+
+    #salary=int(emp[3])
+    #total=total+salary
+#print(total)
+
+    #sallist.append(int(emp[3]))
+#print(max(sallist))
+
+
--- pythoncollections/listprograms/pairs.py
+++ pythoncollections/listprograms/pairs.py
@@ -1,2 +1,12 @@
 lst=[1,2,3,4]
-num
+inp=int(input("enter number"))
+low=0
+upp=len(lst)-1
+for i in range(0,len(lst)):
+    if (sum==inp):
+        print("pairs",lst[low],lst[upp])
+        low+=1
+    elif (sum<inp):
+        low+=1
+    else:
+        upp-=1
